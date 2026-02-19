170f9144fd785fb5379aebb13d9db1b0cab11269
scar-blanky <scar.blanky@gmail.com>
2026-02-20 01:14:09 +0800

    add colorscheme rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,43 @@
 return {
-    'catppuccin/nvim',
-    lazy = false,
-    priority = 1001,
-    name = 'catppuccin',
-    opts = {
-        flavour = 'mocha',
-        transparent_background = true,
-        integrations = {
-            cmp = true,
-            lsp_trouble = true,
-            mason = true,
-            native_lsp = {
-                enabled = true,
-                underlines = {
-                    errors = { 'undercurl' },
-                    hints = { 'undercurl' },
-                    warnings = { 'undercurl' },
-                    information = { 'undercurl' },
+    {
+        'rose-pine/neovim',
+        lazy = false,
+        priority = 1001,
+        name = 'rose-pine',
+        opts = {
+            dark_variant = 'moon',
+            styles = {
+                transparency = true,
+            }
+        },
+    },
+
+    {
+        'catppuccin/nvim',
+        lazy = false,
+        priority = 1001,
+        name = 'catppuccin',
+        opts = {
+            flavour = 'mocha',
+            transparent_background = true,
+            integrations = {
+                cmp = true,
+                lsp_trouble = true,
+                mason = true,
+                native_lsp = {
+                    enabled = true,
+                    underlines = {
+                        errors = { 'undercurl' },
+                        hints = { 'undercurl' },
+                        warnings = { 'undercurl' },
+                        information = { 'undercurl' },
+                    },
                 },
+                telescope = true,
+                treesitter = true,
+                treesitter_context = true,
+                which_key = true,
             },
-            telescope = true,
-            treesitter = true,
-            treesitter_context = true,
-            which_key = true,
         },
     },
 }
